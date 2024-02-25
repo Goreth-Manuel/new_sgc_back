b7b8506c936f6f7e59941c85ded1188f61f9f3f5
Goreth Manuel <gorethmanuel0@gmail.com>
2024-02-25 16:42:33 +0100

    Adicona o orquivo elephnatsql

---
+++ elephantsql.js
@@ -0,0 +1,17 @@
+var pg = require('pg');
+
+
+var conString = "INSERT_YOUR_POSTGRES_URL_HERE" 
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    client.end();
+  });
+});
